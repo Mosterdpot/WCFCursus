8d9971c9ea3add6ce1d3c1b26dabcdf2fb097e31
Mosterdpot <jeroen.merckx@gmail.com>
2016-09-02 09:26:00 +0200

    add operationcontract attr

--- BierenServiceLibrary/IBierenService.cs
+++ BierenServiceLibrary/IBierenService.cs
@@ -10,8 +10,11 @@ namespace BierenServiceLibrary
     [ServiceContract(Namespace="http://vdab.be/bierenservice")]
     public interface IBierenService
     {
+        [OperationContract]
         int GetTotaalAantalBieren();
+        [OperationContract]
         int GetAantalBierenTussenAlcohol(decimal van, decimal tot);
+        [OperationContract]
         List<Bier> GetBierenMetWoord(string woord);
     }
 }
