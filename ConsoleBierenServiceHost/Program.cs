3d761e93048004ae18eaa61f29a38016e75781bb
Mosterdpot <jeroen.merckx@gmail.com>
2016-09-02 11:15:56 +0200

    switch to selfhost in console

---
+++ ConsoleBierenServiceHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using BierenServiceLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBierenServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var serviceHost = new ServiceHost(typeof(BierenService)))
+            {
+                serviceHost.Open();
+                Console.WriteLine("Druk s om de service te stoppen");
+                while (Console.ReadLine() != "s")
+                {
+
+                }
+            }
+        }
+    }
+}
